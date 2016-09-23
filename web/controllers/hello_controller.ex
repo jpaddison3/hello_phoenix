23abacba39e03febcbcfe09c864cc5883237e080
JP Addison <johnpaddison@gmail.com>
2016-09-22 20:43:18 -0700

    add web code pt1

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,12 @@
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"messenger" => message}) do
+    render conn, "show.html", messenger: message
+  end
+end
+
